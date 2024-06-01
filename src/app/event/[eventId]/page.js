04f7ce22eaacf4b25116f29fcb43d3787141804c
Mahardhika Pratama <putrad578@gmail.com>
2024-06-01 13:19:54 +0700

    feat: add metadata google

--- src/app/event/[eventId]/page.js
+++ src/app/event/[eventId]/page.js
@@ -1,14 +1,16 @@
-'use client';
-import React, { useEffect, useState } from 'react';
-import Header from '@/components/header/header';
-import Footer from '@/components/footer/footer';
-import { useParams } from 'next/navigation';
-import Image from 'next/image';
-import axios from 'axios';
+"use client";
+import React, { useEffect, useState } from "react";
+import Header from "@/components/header/header";
+import Footer from "@/components/footer/footer";
+import { useParams } from "next/navigation";
+import Image from "next/image";
+import axios from "axios";
+import Head from "next/head";
 
 export default function DetailEvent() {
   const params = useParams();
   const [data, setData] = useState([]);
+  const [dynamicMetadata, setDynamicMetadata] = useState("");
 
   const GetDetailEvent = async (idEvent) => {
     try {
@@ -17,33 +19,55 @@ export default function DetailEvent() {
       );
       return response.data; // Mengembalikan data yang diterima dari panggilan API
     } catch (error) {
-      console.error('Error fetching data:', error);
+      console.error("Error fetching data:", error);
       throw error; // Melemparkan error untuk menangani di tempat lain jika perlu
     }
+  };
+
+  const generateMetadata = async (eventData) => {
+    // Menghasilkan metadata berdasarkan data acara yang diterima dari panggilan API
+    return {
+      title: truncateText(eventData.post_article, 60),
+      description: truncateText(eventData.post_article, 160),
+      // Tambahkan metadata lain sesuai kebutuhan
+    };
   };
 
   const getDetailEvent = async () => {
     try {
       const response = await GetDetailEvent(params.eventId);
       setData(response?.data);
+
+      // Set metadata dinamis setelah menerima data dari API
+      const dynamicMetadata = await generateMetadata(response?.data);
+      setDynamicMetadata(dynamicMetadata);
     } catch (error) {
-      console.error('Error get detail event', error);
+      console.error("Error get detail event", error);
     }
   };
 
   const formatDateInIndonesiaTime = (dateStr) => {
     const date = new Date(dateStr);
     const options = {
-      weekday: 'long',
-      day: '2-digit',
-      month: 'long',
-      year: 'numeric',
-      hour: '2-digit',
-      minute: '2-digit',
-      timeZone: 'Asia/Jakarta',
-      timeZoneName: 'short',
+      weekday: "long",
+      day: "2-digit",
+      month: "long",
+      year: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+      timeZone: "Asia/Jakarta",
+      timeZoneName: "short",
     };
-    return date.toLocaleDateString('id-ID', options).replace('pukul', '|');
+    return date.toLocaleDateString("id-ID", options).replace("pukul", "|");
+  };
+
+  const truncateText = (text, maxLength) => {
+    // Remove HTML tags from text
+    const plainText = text?.replace(/<[^>]*>/g, " ");
+    if (plainText?.length > maxLength) {
+      return plainText.substring(0, maxLength) + "...";
+    }
+    return plainText;
   };
 
   useEffect(() => {
@@ -51,6 +75,11 @@ export default function DetailEvent() {
   }, []);
   return (
     <>
+      <Head>
+        <title>{dynamicMetadata?.title}</title>
+        <meta name="description" content={dynamicMetadata?.description} />
+        <meta name="google-site-verification" content="qrhjx4oG31-_WzMue_uckzsud9098vqZz3s7YbKJgDM" />
+      </Head>
       <div className="bg-white">
         <div className="sticky top-0 bg-[#fad74f] rounded-b-3xl z-10">
           <div className="container w-11/12 sm:w-10/12 mx-auto">
@@ -64,8 +93,8 @@ export default function DetailEvent() {
           <div className="flex justify-center my-2">
             <Image
               src={
-                data?.image === ('null' || null || undefined || 'undefined')
-                  ? '/images/noimage.png'
+                data?.image === ("null" || null || undefined || "undefined")
+                  ? "/images/noimage.png"
                   : data?.image
               }
               className="w-full h-70vw sm:h-50vw lg:h-[640px] bg-slate-200 rounded-xl object-cover border shadow-sm"
@@ -76,7 +105,7 @@ export default function DetailEvent() {
           </div>
           <div className="mb-10">
             <p className="font-medium">
-              {formatDateInIndonesiaTime(data?.created_at)} -{' '}
+              {formatDateInIndonesiaTime(data?.created_at)} -{" "}
               <span className="text-blue-400">{data?.username}</span>
             </p>
           </div>
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,22 +1,25 @@
-import { Inter, Poppins } from 'next/font/google';
-import './globals.css';
+import { Inter, Poppins } from "next/font/google";
+import "./globals.css";
+import Head from "next/head";
 
 // const inter = Inter({ subsets: ['latin'] });
 const poppins = Poppins({
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-  subsets: ['latin'],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
 });
-
-export const metadata = {
-  title: 'Perisai Diri Cabang Sleman',
-  description: 'Pandai silat, tanpa cidera',
-};
 
 export default function RootLayout({ children }) {
   return (
-    // bg-[#f8f1ec]
-    <html lang="en" className="bg-[#f6f8fa]">
-      <body className={poppins.className}>{children}</body>
-    </html>
+    <>
+      <Head>
+        <title>Perisai Diri Cabang Sleman</title>
+        <meta name="description" content="Pandai silat, tanpa cidera"/>
+        <meta name="google-site-verification" content="qrhjx4oG31-_WzMue_uckzsud9098vqZz3s7YbKJgDM" />
+        {/* Add other metadata tags here */}
+      </Head>
+      <html lang="en" className="bg-[#f6f8fa]">
+        <body className={poppins.className}>{children}</body>
+      </html>
+    </>
   );
 }
